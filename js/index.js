5bcc4c5e651681b0a4576358e4358809c125a78b
cquel-2021006 <cquel-2021006@kinal.edu.gt>
2023-01-20 09:32:37 -0600

    Primer commit v1 webpage

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function Calcular(){
+    let numA = document.getElementById("no1").value
+    let numB = document.getElementById("no2").value
+    let suma = parseInt(numA) + parseInt(numB)
+
+    console.log("Saludos")
+    console.log("La suma es " + suma)
+}
+
